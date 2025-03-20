8c9e50c5520475a8554d07269290d0549914aa45
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-03-20 16:17:53 +0100

    add main section

---
+++ src/components/Main.jsx
@@ -0,0 +1,43 @@
+export default function Main() {
+  return (
+    <main className="main">
+      <div className="content-container">
+        <h2>CONTENT</h2>
+      </div>
+      <div className="feature-menu">
+        <ul>
+          <li>
+            <a href="DIGITAL COMICS">
+              <img src="./buy-comics-digital-comics.png" alt="digital comics" />
+              <p>digital comics</p>
+            </a>
+          </li>
+          <li>
+            <a href="MERCHANDISE">
+              <img src="./buy-comics-merchandise.png" alt="dc merchandise" />
+              <p>dc merchandise</p>
+            </a>
+          </li>
+          <li>
+            <a href="SUBSCRIPTIONS">
+              <img src="./buy-comics-subscriptions.png" alt="subscriptions" />
+              <p>subscriptions</p>
+            </a>
+          </li>
+          <li>
+            <a href="SHOP LOCATOR">
+              <img src="./buy-comics-shop-locator.png" alt="shop locator" />
+              <p>comic shop locator</p>
+            </a>
+          </li>
+          <li>
+            <a href="POWER VISA">
+              <img src="./buy-dc-power-visa.svg" alt="power visa" />
+              <p>dc power visa</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
