6724fabef7801890854b02d03a15fbbee729b989
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-03-20 15:11:15 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./dc-logo.png" alt="logo" />
+      <ul>
+        <li>
+          <a href="CHARACTERS">CHARACTERS</a>
+        </li>
+        <li>
+          <a href="COMICS">COMICS</a>
+        </li>
+        <li>
+          <a href="MOVIES">MOVIES</a>
+        </li>
+        <li>
+          <a href="TV">TV</a>
+        </li>
+        <li>
+          <a href="GAMES">GAMES</a>
+        </li>
+        <li>
+          <a href="COLLECTIBLES">COLLECTIBLES</a>
+        </li>
+        <li>
+          <a href="VIDEOS">VIDEOS</a>
+        </li>
+        <li>
+          <a href="FANS">FANS</a>
+        </li>
+        <li>
+          <a href="NEWS">NEWS</a>
+        </li>
+        <li>
+          <a href="SHOP">SHOP</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
